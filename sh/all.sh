4afca1689441bc6c8e973074fd1abb75320b435c
Matthew Sainsbury-Dale <msdale@uow.edu.au>
2023-11-18 10:35:08 +1100

    Cleaned console output in .sh scripts

--- sh/all.sh
+++ sh/all.sh
@@ -15,12 +15,10 @@ if ! [[ $quick_str == "y" ||  $quick_str == "Y" || $quick_str == "n" ||  $quick_
     exit 1
 fi
 
-# Each .sh files asks the user if quick = y/n. To automate this
-# script, we pipe the above response to each .sh file
-Rscript src/pointprocesses.R                          # Point process figure in Section 2
-yes $quick_str | bash sh/simulationstudies.sh     # Section 3
-yes $quick_str | bash sh/supplement.sh            # Supplementary material
-yes $quick_str | bash sh/application.sh           # Section 4
+Rscript src/pointprocesses.R       # Point process figure in Section 2
+source sh/simulationstudies.sh     # Section 3
+source sh/supplement.sh            # Supplementary material
+source sh/application.sh           # Section 4
 
 echo ""
 echo "######## Everything finished! ############"
--- sh/supplement.sh
+++ sh/supplement.sh
@@ -3,8 +3,10 @@ unset R_HOME
 
 set -e
 
-echo "Do you wish to use a very low number of parameter configurations and epochs to quickly establish that the code is working? (y/n)"
-read quick_str
+if [[ ! -v quick_str ]]; then
+  echo "Do you wish to use a very low number of parameter configurations and epochs to quickly establish that the code is working? (y/n)"
+  read quick_str
+fi
 
 if [[ $quick_str == "y" ||  $quick_str == "Y" ]]; then
     quick=--quick
